1776b6d4771e6d75ba448b9211898de2ef7e60fa
Rajan Chaudhary <rajann44@gmail.com>
2023-04-12 20:22:23 +0200

    login page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import ProductDetail from "./components/ProductDetail";
 import { Route, Routes } from "react-router-dom";
 import Checkout from "./components/Checkout";
 import Banner from "./components/Banner";
+import Login from "./components/Login";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
           path="/product"
           element={<ProductDetail></ProductDetail>}
         ></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
         <Route path="/checkout" element={<Checkout></Checkout>}></Route>
       </Routes>
       <Footer></Footer>
