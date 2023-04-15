1e511eab0080f52b1dda3300e1fe7dd99013becf
Rajan Chaudhary <60035342+rajann44@users.noreply.github.com>
2023-04-16 00:16:57 +0200

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -35,7 +35,7 @@ const Footer = () => {
             rel="noopener noreferrer"
             target="_blank"
           >
-            @rajann44
+            @rajan
           </a>
         </p>
         <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
